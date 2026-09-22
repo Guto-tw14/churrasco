285cf09a3bfd546bdcb41b6948b09ffaaf6e9c33
AUGUSTO WAINER <augusto.twainer@gmail.com>
2026-09-22 10:39:59 -0300

    cadastro

---
+++ participantes/cadastrar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <label>
+            <span>Nome</span>
+            <input type="text" name="nome" required>
+        </label>
+        <label>
+            <span>Turma</span>
+            <input type="text" name="turma" required>
+        </label>
+        <label>
+            <span>Telefone</span>
+            <input type="text" name="telefone" required>
+        </label>
+        <label>
+            <span>Tipo de churrasco</span>
+            <input type="text" name="tipo" required>
+        </label>
+        <label>
+            <span>Acompanhamento</span>
+            <input type="text" name="acompanhamento" required>
+        </label>
+        <label>
+            <span>Presença confirmada</span>
+            <input type="checkbox" name="presenca">
+        </label>
+        <label>
+            <span>Pagamento realizado</span>
+            <input type="checkbox" name="pagamento">
+        </label>
+    </form>
+</body>
+</html>
