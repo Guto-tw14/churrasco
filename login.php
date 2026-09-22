cc558e9c141b9276ced24a09b18f5f550236c2a5
AUGUSTO WAINER <augusto.twainer@gmail.com>
2026-09-22 08:38:40 -0300

    login

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="autenticar.php" method="POST">
+        <label>
+            <span>email</span>
+            <input type="email" name="email" required>
+        </label>
+        <label>
+            <span>senha</span>
+            <input type="password" name="senha" required>
+        </label>
+        <input type="submit" value="Entrar">
+    </form>
+</body>
+</html>
