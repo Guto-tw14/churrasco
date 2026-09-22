4e8d95a333a2d4cdbba8de8b3a57ca0d01a78b55
AUGUSTO WAINER <augusto.twainer@gmail.com>
2026-09-22 08:36:37 -0300

    iniciando app

---
+++ banco.sql
@@ -0,0 +1,20 @@
+create database churrasco;
+use churrasco;
+
+create table usuarios(
+    id int primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null unique,
+    senha varchar(100) not null
+);
+
+create table participantes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    turma VARCHAR(50) NOT NULL,
+    telefone VARCHAR(20),
+    tipo_churrasco VARCHAR(30) NOT NULL,
+    acompanhamento VARCHAR(50),
+    confirmado BOOLEAN NOT NULL
+    pago BOOLEAN NOT NULL
+);
